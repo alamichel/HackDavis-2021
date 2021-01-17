49df9105c78003fb27909b51624a13850c9d265f
alamichel <alamichel@ucdavis.edu>
2021-01-17 15:39:39 -0800

    Rearrange for easier access

---
+++ final-project/background.js
@@ -0,0 +1,63 @@
+console.log('background running');
+
+var meeting = '';
+
+chrome.storage.sync.get('meetingTime',function(zoom){
+    $('#meetingTime').val(zoom.meetingTime);
+    meeting = zoom.meetingTime;
+});
+
+// alt+a command
+chrome.commands.onCommand.addListener(function (command) {
+    console.log('alt+a');
+    var notifOptions = {
+        type: "basic",
+        iconUrl: "icon48.png",
+        title: "Command Test",
+        message: meeting.valueOf()
+    };
+
+    chrome.notifications.create('commandNotif', notifOptions);
+
+});
+
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    if (alarm.data == "Meeting soon") {
+        console.log('Less than 5 seconds till meeting');
+        chrome.notifications.create('timeNotif', {
+            type: "basic",
+            iconUrl: "icon48.png",
+            title: "Meeting soon",
+            message: "Less than 5 seconds till meeting."
+        });
+    }
+});
+
+// alarm function
+
+/*
+    IDEA: Delay timer by the difference between current time and alarm time
+*/
+var now = new Date();
+//var eventTime = new Date(document.getElementById('meetingTime'));
+var eventTime = new Date(meeting.valueOf());
+var alarmTime = new Date(eventTime.valueOf() - now.valueOf() - (5 * 1000)); // event - 5s === event - 5000ms
+chrome.alarms.create("Meeting soon", { delayInMinutes: alarmTime.getMinutes() });
+
+/* var ms = document.getElementById('meetingTime').valueAsNumber; // no access to meetingTime
+var alarm = new Date(ms);
+var alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(), alarm.getUTCDate(), alarm.getUTCHours(), alarm.getUTCMinutes(), alarm.getUTCSeconds());
+var differenceInMs = alarmTime.getTime() - (new Date()).getTime();
+
+if (differenceInMs < 5000) {
+    console.log('5 seconds till meeting');
+    var notifOptions = {
+        type: "basic",
+        iconUrl: "icon48.png",
+        title: "Meeting soon",
+        message: "5 seconds till meeting."
+    };
+
+    chrome.notifications.create('timeNotif', notifOptions);
+};
+ */
---
+++ final-project/popup.js
@@ -0,0 +1,90 @@
+$(function(){
+
+    // Enlightenment:
+    //   chrome.storage.sync - Syncs with user that is logged in
+    //      MAX_ITEMS = 512 (max. num. of items that can be stored in sync storage)
+    //   chrome.storage.local - Stays on local browser/machine
+    //      QUOTA_BYTES = 5242880 (max. amt. in bytes of data that can be stored in local storage);
+    //          ignored if unlimitedStorage permission is enabled
+
+    chrome.storage.sync.get(['meeting_id', 'passcode', 'date_time'], function(agenda){
+        $('#meeting_id').text(agenda.meeting_id);
+        $('#passcode').text(agenda.passcode);
+        $('#date_time').text(agenda.date_time);
+    });
+
+    $('#convert').click(function(){
+        chrome.storage.sync.get(['meeting_id', 'passcode', 'date_time'], function (agenda){
+            
+            // Make text box input accessible
+            var text_input = document.getElementById('message').value;
+
+            // Get Meeting ID (Meeting ID: XXX XXXX XXXX)
+            // IDEA: Save Meeting ID as Number instead of String
+            var id_index = text_input.search("Meeting ID: ") + 12;
+            if (id_index - 12 > -1){
+                // Read in ID and store for this meeting
+                var id_content = text_input.substr(id_index, 13);
+                $('#meeting_id').text(id_content);
+            } else {
+                // Missing keywords; assume no ID given
+                $('#meeting_id').text("NOT FOUND");
+            }
+            
+            // Get Passcode (Passcode: [Until a space is reached]) (10char max, 1char min)
+            var pw_index = text_input.search("Passcode: ") + 10;
+            if (pw_index - 10 > -1){
+                var buf_char = '';  // Character buffer to store passcode and signal stop
+                var pw_content = '';
+                for (var i = 0; i < 10; i++){
+                    // If char @cur_pos, cat to output; break when we reach end of passcode
+                    if (i === 0) {
+                        pw_content = text_input.charAt(pw_index);
+                    } else {
+                        buf_char = text_input.charAt(pw_index + i);
+                        if (buf_char.includes(' ') || buf_char.includes('\0')) {
+                            break;
+                        } else {
+                            pw_content += buf_char;
+                        }
+                    }
+                }
+                // Store passcode and display to user
+                $('#passcode').text(pw_content);
+            } else {
+                // No password found; leave blank (to avoid confusion)
+                $('#passcode').text('');
+            }
+
+            // Get Date & Time (HINT: All months are abbreviated to three letters)
+            var date_array = '';
+            var date_content = [];
+            var date_convert = '';
+            //  General Form: [3-ltr month] [1 or 2-num day], [4-num year] [AM/PM]
+            var date_regex = /\D{3}\s\d\d?,\s\d{4}\s\d\d?:\d{2}\s[AP]M/gm;
+            var date_index = text_input.search("Time: ");
+            if (date_index < 0) {
+                // Missing keyword; ignore invite dates
+                $('#date_time').text('');
+            } else {
+                // Look for all dates and times
+                date_array = text_input.match(date_regex);
+
+                if (date_array.length === 1) {
+                    // Only one date/time found
+                    date_content = new Date(date_array[0]);
+                    $('#date_time').text(date_content);
+                } else if (date_array.length > 1) {
+                    // Multiple dates/times found; ignore the first one as it is repeated later
+                    for (var i = 1; i < date_array.length; i++) {
+                        date_convert = new Date(date_array[i]);
+                        date_content.push(date_convert);
+                    }
+
+                    // Print out the first date/time and the number of remaining occurances
+                    $('#date_time').text(date_content[1] + ' and ' + (date_content.length - 1) + ' other occurance(s).');
+                }
+            }
+        }); 
+    });
+});
