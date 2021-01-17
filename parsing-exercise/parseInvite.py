35a393ed2ae55d9ed6353de65eceab550f7561d8
Cai, Jessica <jessica7cai@gmail.com>
2021-01-17 09:43:57 -0800

    parseFunctionPython

---
+++ parsing-exercise/parseInvite.py
@@ -0,0 +1,59 @@
+import re
+import datetime as dt
+
+topic = ''
+occurences = []
+meetingLink = ''
+meetingID = ''
+password = ''
+timeZone = ''
+
+# Update this path to whatever it needs to be
+with open('C:\\Users\\jessi\\OneDrive - University of California, Davis\\programs\\hackDavis21\\test1.txt', "r") as invite:
+    lines = invite.readlines()
+    for line in lines:
+        # Remove any leading/traiing whitespace
+        line = line.strip()
+        # Skip any blank lines
+        if not line:
+            continue
+        else:
+            match = re.search("\d\d:\d\d", line)
+            # Check for topic
+            if re.compile('Topic:').match(line):
+                topic = line[6:].strip()
+                continue
+            # Check for time
+            elif match is not None:
+                plus12 = False
+                pm = re.search("\d\d:\d\d PM", line)
+                if pm is not None:
+                    plus12 = True
+                if re.compile('Time:').match(line) or re.compile('Date Time:').match(line):
+                    line = line[5:].strip()
+                    amPm = re.search("\d\d:\d\d [AP]M", line)
+                    if amPm is not None:
+                        timeZone = line[amPm.end():].strip()
+                    else:
+                        time = re.search("\d\d:\d\d", line)
+                        timeZone = line[time.end():].strip()
+                amPm = re.search("\d\d:\d\d [AP]M", line)
+                if amPm is not None:
+                    line = line[:amPm.end()-2].strip()
+                # Jun 28 2018 07:40
+                dtObject = dt.datetime.strptime(
+                    line, '%b %d, %Y %H:%M')
+                if plus12:
+                    dtObject = dtObject.replace(
+                        hour=dtObject.hour + 12)
+                occurences.append(dtObject)
+            elif re.compile('Every').match(line):
+                # clear original time, since that will be included in the list of dates/times following this line
+                occurences.clear()
+
+print(topic)
+print(timeZone)
+print(occurences)
+#meetingLink = ''
+#meetingID = ''
+#password = ''
